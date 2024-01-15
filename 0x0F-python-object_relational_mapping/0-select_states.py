5c41fbc2cc0c696980b823b7a432b6cb0bd072e5
omar546 <omer.mkawy.om@gmail.com>
2024-01-15 19:02:26 +0200

    0-3

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""List states"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    connection = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                           passwd=argv[2], db=argv[3], charset="utf8")
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
+    result = cursor.fetchall()
+    for line in result:
+        print(line)
+    cursor.close()
+    connection.close()
+    
---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""List states start with N"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    connection = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                           passwd=argv[2], db=argv[3], charset="utf8")
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
+    result = cursor.fetchall()
+    for line in result:
+        if line[1].startswith("N"):
+            print(line)
+    cursor.close()
+    connection.close()
+    
