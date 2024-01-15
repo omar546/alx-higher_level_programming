5dba828d983d365c16a1e839f5de9c440e085a91
omar546 <omer.mkawy.om@gmail.com>
2024-01-15 19:31:01 +0200

    5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -9,14 +9,8 @@ if __name__ == "__main__":
                            passwd=argv[2], db=argv[3], charset="utf8")
     cursor = connection.cursor()
     cursor.execute("""
-            SELECT
-                cities.id, cities.name, states.name
-            FROM
-                cities
-            JOIN
-                states
-            ON
-                cities.state_id = states.id
+            SELECT cities.name FROM cities
+            JOIN states ON cities.state_id = states.id
             WHERE states.name LIKE BINARY %s
             ORDER BY
                 cities.id ASC
