66bf9b5cf76e853d3d5e501cf41fa72d312dbfb3
omar546 <omer.mkawy.om@gmail.com>
2024-01-15 19:23:49 +0200

    5

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""List cities by user input state in database"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    connection = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                           passwd=argv[2], db=argv[3], charset="utf8")
+    cursor = connection.cursor()
+    cursor.execute("""
+            SELECT
+                cities.id, cities.name, states.name
+            FROM
+                cities
+            JOIN
+                states
+            ON
+                cities.state_id = states.id
+            WHERE states.name LIKE BINARY %s
+            ORDER BY
+                cities.id ASC
+        """,argv[4])
+    result = cursor.fetchall()
+    if result is not None:
+        for line in result:
+            print(line)
+
+    cursor.close()
+    connection.close()
+    
