c55976c6440053aca090bfae966624fed117538a
omar546 <omer.mkawy.om@gmail.com>
2024-02-03 00:05:44 +0200

    3.1

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,18 +1,24 @@
 #!/usr/bin/python3
-"""List states by user input (safe)"""
-
+"""retrive all states"""
 import MySQLdb
-from sys import argv
+import sys
 
-if __name__ == "__main__":
-    connection = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                           passwd=argv[2], db=argv[3], charset="utf8")
-    cursor = connection.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY %s ORDER BY states.id ASC",argv[4])
-    result = cursor.fetchall()
-    if result is not None:
-        for line in result:
-            print(line)
-    cursor.close()
-    connection.close()
-    
+
+if __name__ == '__main__':
+    args = sys.argv
+    if len(args) < 5:
+        print("Usage: {} username password db_name state_name".format(args[0]))
+        exit(1)
+    name = args[1]
+    passwd = args[2]
+    dbName = args[3]
+    state_name = args[4]
+    dataBase = MySQLdb.connect(host='localhost', user=name,
+                         passwd=passwd, db=dbName,
+                         port=3306)
+    cursor = dataBase.cursor()
+    nrows = cursor.execute("SELECT * FROM states ORDER BY states.id")
+    all = cursor.fetchall()
+    for one in all:
+        if (state_name == one[1]):
+            print(one)
