afb04656fd087b39a1d5a0e22e53ee5dbc05d071
omar546 <omer.mkawy.om@gmail.com>
2024-02-03 18:41:55 +0200

    g

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,7 +3,6 @@
 
 def find_peak(list_of_integers):
     """Return the peak of a list of integers."""
-
     size = len(list_of_integers)
     if size == 0:
         return None
@@ -11,11 +10,10 @@ def find_peak(list_of_integers):
         return list_of_integers[0]
     elif size == 2:
         return max(list_of_integers)
-
     middle = int(size / 2)
     peak = list_of_integers[middle]
-    if peak > list_of_integers[middle + 1] \
-            and peak > list_of_integers[middle - 1]:
+    if peak > list_of_integers[middle + 1]\
+         and peak > list_of_integers[middle - 1]:
         return peak
     elif peak < list_of_integers[middle - 1]:
         return find_peak(list_of_integers[:middle])
