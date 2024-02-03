2c79bc552932a3a46b51c888f42daa6365469890
omar546 <omer.mkawy.om@gmail.com>
2024-02-03 02:37:50 +0200

    5.1.1

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -6,18 +6,17 @@ from sys import argv
 
 if __name__ == "__main__":
     connection = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                           passwd=argv[2], db=argv[3], charset="utf8")
+                                 passwd=argv[2], db=argv[3], charset="utf8")
     cursor = connection.cursor()
     cursor.execute("""
-            SELECT cities.name FROM cities
-            JOIN states ON cities.state_id = states.id
-            WHERE states.name LIKE BINARY %s
-            ORDER BY
-                cities.id ASC
-        """,(argv[4], ))
+                   SELECT cities.name FROM cities
+                   JOIN states ON cities.state_id = states.id
+                   WHERE states.name LIKE BINARY %s
+                   ORDER BY
+                   cities.id ASC
+                   """, (argv[4], ))
     result = cursor.fetchall()
     if result is not None:
         print(", ".join(map(lambda x: x[0], result)))
     cursor.close()
     connection.close()
-    
