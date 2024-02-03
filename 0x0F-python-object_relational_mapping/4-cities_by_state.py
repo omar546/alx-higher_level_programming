7e0d0bc67e14ff7853a9091150c956ca91f7e772
omar546 <omer.mkawy.om@gmail.com>
2024-02-03 02:35:02 +0200

    4.1.1

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -6,20 +6,21 @@ from sys import argv
 
 if __name__ == "__main__":
     connection = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                           passwd=argv[2], db=argv[3], charset="utf8")
+                                 passwd=argv[2], db=argv[3], charset="utf8")
     cursor = connection.cursor()
     cursor.execute("""
-            SELECT
-                cities.id, cities.name, states.name
-            FROM
-                cities
-            JOIN
-                states
-            ON
-                cities.state_id = states.id
-            ORDER BY
-                cities.id ASC
-        """)
+                   SELECT
+                   cities.id, cities.name, states.name
+                   FROM
+                   cities
+                   
+                JOIN
+                   states
+                   ON
+                   cities.state_id = states.id
+                   ORDER BY
+                   cities.id ASC
+                   """)
     result = cursor.fetchall()
     if result is not None:
         for line in result:
@@ -27,4 +28,3 @@ if __name__ == "__main__":
 
     cursor.close()
     connection.close()
-    
