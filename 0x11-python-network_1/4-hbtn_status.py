3ac2a574ddd77bdd83501651ef9fb20415243b07
omar546 <omer.mkawy.om@gmail.com>
2024-02-03 19:12:28 +0200

    4.1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-"""testing status of web page"""
+"""Fetches status"""
+import requests
+
 if __name__ == "__main__":
-    import requests
-    url = "https://intranet.hbtn.io/status"
-    response = requests.get(url)
-    content = response.text
-    print_str = '''Body response:
-\t- type: {}
-\t- content: {}'''.format(type(content), content)
-    print(print_str)
+    r = requests.get("https://alx-intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
