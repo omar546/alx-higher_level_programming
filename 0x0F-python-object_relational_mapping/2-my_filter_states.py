745d22f207e69ca1c182b43287ab82215613fc25
omar546 <omer.mkawy.om@gmail.com>
2024-02-03 02:32:17 +0200

    2.1.1

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -6,15 +6,14 @@ from sys import argv
 
 if __name__ == "__main__":
     connection = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                           passwd=argv[2], db=argv[3], charset="utf8")
+                                 passwd=argv[2], db=argv[3], charset="utf8")
     cursor = connection.cursor()
     cursor.execute("SELECT * FROM states \
-                 WHERE name LIKE BINARY '{}' \
-                 ORDER BY states.id ASC".format(argv[4]))
+                   WHERE name LIKE BINARY '{}' \
+                   ORDER BY states.id ASC".format(argv[4]))
     result = cursor.fetchall()
     for line in result:
         if line[1].startswith("N"):
             print(line)
     cursor.close()
     connection.close()
-    
