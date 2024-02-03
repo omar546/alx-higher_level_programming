7602867e98e268708e4b742d6023f15d27843931
omar546 <omer.mkawy.om@gmail.com>
2024-02-03 18:26:49 +0200

    0.1

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-#This script accepts a URL as input,
-#initiates a request to the specified URL,
-#and presents the size of the response body.
+# presents the size of the response body.
 curl -sI "$1" | grep "Content-Length:" | cut -d' ' -f2
